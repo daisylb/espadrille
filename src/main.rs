89412109b7135f2a4b7b7d5f1f310c44112af5dc
Adam Brenecki <adam@brenecki.id.au>
2018-11-14 17:21:34 +1030

    Switch from using strings to path types for paths

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ use crypto::digest::Digest;
 use crypto::sha3::Sha3;
 use std::fs;
 use std::io;
+use std::path::{Path, PathBuf};
 use std::process;
 
-fn dir_exists(path: &str) -> io::Result<bool> {
+fn dir_exists(path: &Path) -> io::Result<bool> {
     match fs::metadata(path) {
         Err(e) => {
             if e.kind() == io::ErrorKind::NotFound {
@@ -27,7 +28,10 @@ fn dir_exists(path: &str) -> io::Result<bool> {
             } else {
                 Err(io::Error::new(
                     io::ErrorKind::AlreadyExists,
-                    format!("{} exists and is not a directory", path),
+                    format!(
+                        "{} exists and is not a directory",
+                        path.to_str().unwrap_or("<unprintable path>")
+                    ),
                 ))
             }
         }
@@ -56,22 +60,22 @@ fn main() {
     hasher.input_str(&hash_value);
     let hash = hasher.result_str();
 
-    let ve_path = format!(
-        "{}/pyscript/envs/{}",
-        dirs::cache_dir().unwrap().to_str().unwrap(),
-        hash
-    );
+    let ve_path: PathBuf = dirs::cache_dir()
+        .unwrap()
+        .join("pyscript")
+        .join("envs")
+        .join(hash);
     if !dir_exists(&ve_path).unwrap() {
         fs::create_dir_all(&ve_path).unwrap();
         process::Command::new("python3")
             .arg("-m")
             .arg("venv")
-            .arg(&ve_path)
+            .arg(ve_path.as_os_str())
             .spawn()
             .unwrap()
             .wait()
             .unwrap();
-        process::Command::new(format!("{}/bin/pip", &ve_path))
+        process::Command::new(ve_path.join("bin").join("pip").as_os_str())
             .arg("install")
             .args(dependencies)
             .spawn()
@@ -79,7 +83,7 @@ fn main() {
             .wait()
             .unwrap();
     }
-    process::Command::new(format!("{}/bin/python", &ve_path))
+    process::Command::new(ve_path.join("bin").join("python").as_os_str())
         .args(matches.values_of("python_args").unwrap())
         .spawn()
         .unwrap()
